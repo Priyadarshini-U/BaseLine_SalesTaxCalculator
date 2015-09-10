746866a18aca46e7a97635f4a41f965079182160
priyadarshini <priyadr@thoughtworks.com>
2015-09-10 12:04:30 +0530

    removing type check out of good

--- src/com/tw/shopping/Good.java
+++ src/com/tw/shopping/Good.java
@@ -10,22 +10,6 @@ public class Good {
         this.price = price;
     }
 
-    public boolean isFood() {
-        if (name.contains("chocolate"))
-            return true;
-        return false;
-    }
-
-    public boolean isBook() {
-        if (name.contains("book"))
-            return true;
-        return false;
-    }
-
-    public boolean isMedicine() {
-        return false;
-    }
-
     public String getName() {
         return name;
     }
--- test/com.tw.shopping/GoodTest.java
+++ test/com.tw.shopping/GoodTest.java
@@ -10,42 +10,6 @@ import static org.junit.Assert.assertTrue;
 public class GoodTest {
 
     @Test
-    public void shouldNotReturnCDAsFood() {
-        Good good = new Good("CD", 3.2);
-        assertFalse(good.isFood());
-    }
-
-    @Test
-    public void shouldReturnChocolateAsFood() {
-        Good good = new Good("chocolate", 3.2);
-        assertTrue(good.isFood());
-    }
-
-    @Test
-    public void shouldReturnAnyGoodThatHasChocolateInItsNameAsFood() {
-        Good good = new Good("chocolate bar", 3.2);
-        assertTrue(good.isFood());
-    }
-
-    @Test
-    public void shouldNotReturnCDAsBook() {
-        Good good = new Good("CD", 3.2);
-        assertFalse(good.isBook());
-    }
-
-    @Test
-    public void shouldReturnAnyGoodThatHasBookInItsNameAsBook() {
-        Good good = new Good("chocolate book", 3.2);
-        assertTrue(good.isBook());
-    }
-
-    @Test
-    public void shouldNotReturnCDAsMedicine() {
-        Good good = new Good("CD", 3.2);
-        assertFalse(good.isMedicine());
-    }
-
-    @Test
     public void shouldHaveSalesTaxAsTenPercent() {
         Good good = new Good("CD", 30.2);
         assertEquals(good.salesTax(), 30.2/10, 0.0);
